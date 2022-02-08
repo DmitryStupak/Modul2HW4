1d506baceb0b9702e648a2ba4ca56bc2f9e28068
DmitryStupak <dimast_ukr@outlook.com>
2022-02-08 14:04:33 +0200

    First commit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,47 @@
 
 namespace Modul2HW4
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SafariPark safariPark = new SafariPark();
+            Console.WriteLine("До сортировки :");
+            foreach (var item in safariPark.Section)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Количество животных в секции : {safariPark.GetCountAnimal()}");
+            Console.WriteLine($"############################################################");
+
+            safariPark.Section.Sort();
+            Console.WriteLine("После сортировки по имени :");
+            foreach (var item in safariPark.Section)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Количество животных в секции : {safariPark.GetCountAnimal()}");
+
+            Console.WriteLine($"############################################################");
+
+            Console.WriteLine("Поиск животных от 10 до 100 кг :");
+            foreach (var item in safariPark.Section.FindByWeight(10, 100))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Количество животных в секции с заданными параметрами {safariPark.Section.FindByWeight(10, 100).Count}");
+            safariPark.Section.Sort(new CompareAnimal());
+            Console.WriteLine($"############################################################");
+            Console.WriteLine("После сортировки по весу :");
+            foreach (var item in safariPark.Section)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Количество животных в секции : {safariPark.GetCountAnimal()}");
         }
     }
 }
